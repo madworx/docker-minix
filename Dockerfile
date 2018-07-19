aa0b89beb8368a0d66a247c54363821a231faa8d
Martin Kjellstrand <martin.kjellstrand@madworx.se>
2018-07-19 10:09:44 +0200

    Implemented SSH support for image.

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,37 @@
 FROM madworx/qemu AS build
 
 MAINTAINER Martin Kjellstrand [https://github.com/madworx]
+
+RUN qemu-img create -f qcow2 /minix.qcow2 1G
+
 RUN curl 'http://download.minix3.org/iso/minix_R3.3.0-588a35b.iso.bz2' | bzip2 -cd > minix.iso
+COPY tools/patch-image.pl /
+RUN apk add --no-cache perl expect
+RUN ./patch-image.pl minix.iso
 
-COPY tools/install-minix.expect tools/patch-image.pl /
-RUN apk add --no-cache perl expect \
-    && ./patch-image.pl minix.iso
+COPY tools/install-minix.expect /
+RUN /install-minix.expect
 
-RUN echo -en "\0" | dd if=/dev/stdin of=minix.img bs=1024k seek=1024 count=1 \
-    && /install-minix.expect \
-    && qemu-img convert -f raw -O qcow2 -c /minix.img /minix.qcow2
+RUN ssh-keygen -f /root/.ssh/id_rsa -N ''
+COPY tools/install-minix-stage2.expect /
+RUN /install-minix-stage2.expect
+
+COPY tools/install-minix-stage3.expect /
+RUN /install-minix-stage3.expect
+RUN sed 's#^#localhost,127.0.0.1 #' /tmp/pubkeys.minix > /root/.ssh/known_hosts
+
+RUN mkdir -p /target/root
+COPY tools/sshexec.sh            /target/usr/bin/sshexec
+COPY tools/docker-entrypoint.sh  /target/
+RUN cp /minix.qcow2              /target/
+RUN cp -a /root/.ssh             /target/root/
+RUN ls -lhR /target
+RUN ls -lh /
 
 FROM madworx/qemu
-COPY --from=build /minix.qcow2 /
-ENTRYPOINT [ "qemu-system-x86_64", "-nographic", "-boot", "c", "-serial", "mon:stdio", "-hda", "/minix.qcow2" ]
+ENV SYSTEM_MEMORY=512 \
+    SYSTEM_CPUS=1 \
+    SSH_PORT=22
+COPY --from=build /target /
+RUN ls -lh /
+ENTRYPOINT [ "/docker-entrypoint.sh" ]
---
+++ tools/docker-entrypoint.sh
@@ -0,0 +1,63 @@
+#! /bin/bash
+
+# Parse command line arguments:
+QUIET=0
+if [ ! -z "$*" ] ; then
+    while [ "$#" -gt 0 ] ; do
+        case "$1" in
+            -q) QUIET=$(($QUIET+1)) ; shift ;;
+            -*) echo "Unknown option \`$1'." ; exit 1 ;;
+            *) QUIET=$(($QUIET+1)) ; break ;;
+        esac
+    done
+fi
+
+#
+# If we have KVM available, enable it:
+#
+if dd if=/dev/kvm count=0 >/dev/null 2>&1 ; then
+    echo "KVM Hardware acceleration will be used."
+    ENABLE_KVM="-enable-kvm"
+else
+    if [ "${QUIET}" -lt 2 ] ; then
+        echo "Warning: Lacking KVM support - slower(!) emulation will be used." 1>&2
+        sleep 1
+    fi
+    ENABLE_KVM=""
+fi
+
+
+#
+# Shut down gracefully by connecting to the QEMU monitor and issue the
+# shutdown command there.
+#
+trap "{ echo \"Shutting down gracefully...\" 1>&2 ; \
+        echo -e \"system_powerdown\\n\\n\" | nc localhost 4444 ; \
+        wait ; \
+        echo \"Will now exit entrypoint.\" 1>&2 ; \
+        exit 0 ; }" TERM
+
+#
+# Boot up NetBSD by starting QEMU.
+#
+(
+    export QEMU_CMDLINE="-nographic \
+                   -monitor telnet:0.0.0.0:4444,server,nowait \
+                   -boot c \
+                   ${ENABLE_KVM} \
+                   -serial mon:stdio \
+                   -netdev user,id=mynet0,net=192.168.76.0/24,dhcpstart=192.168.76.9,hostfwd=tcp::${SSH_PORT}-:22 -device e1000,netdev=mynet0 \
+                   -hda /minix.qcow2 \
+                   -m ${SYSTEM_MEMORY} -smp ${SYSTEM_CPUS}"
+    case "${QUIET}" in
+        0) exec -a "MINIX3 [QEMU${ENABLE_KVM}]" qemu-system-x86_64 ;;
+        *) exec -a "MINIX3 [QEMU${ENABLE_KVM}]" qemu-system-x86_64 >/dev/null 2>&1 ;;
+    esac
+) &
+
+if [ ! -z "$*" ] ; then
+    /usr/bin/sshexec $*
+    exit $?
+fi
+
+wait
